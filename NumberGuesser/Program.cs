9863261b25da3ceb716b8f6b550ad3b2a7dba053
Carl Kakisis <Messinias1@gmail.com>
2019-09-03 23:05:21 -0400

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Carl Kakisis";
+            int age = 24;
+
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
