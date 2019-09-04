efbe9e2e9167a2ee36653f3eb586afbdb1f369ee
Carl Kakisis <Messinias1@gmail.com>
2019-09-03 23:20:53 -0400

    readline implementation

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -14,7 +14,19 @@ namespace NumberGuesser
             // Change text color
             Console.ForegroundColor = ConsoleColor.Blue;
 
+            // Write Out App Info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset Text Color
+            Console.ResetColor();
+
+            // Ask User's Name
+            Console.WriteLine("What is your name?");
+
+            // Get User input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0} want to play a game?", input);
         }
     }
 }
